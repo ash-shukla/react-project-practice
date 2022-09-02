5eedb391719a2e71dfa3f52a21b9d84151f885b8
Ashish Shukla <shukla21ashish@gmail.com>
2022-09-03 01:43:25 +0530

    form handling done

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,21 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import FormInput from "./FormInput";
 
 function App() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    console.log(Object.fromEntries(data.entries()));
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: "",
+  });
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const data = new FormData(e.target);
+  //   console.log(Object.fromEntries(data.entries()));  // THIS IS AN IMPORTANT WAY OF HANDLING FORM DATA
+  // };
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+      errorMessage:
+        "Username is required and should have atleast 5-10 characters",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      label: "Email",
+      errorMessage: "Please enter a valid email address",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "phone",
+      type: "text",
+      placeholder: "Phone",
+      label: "Phone",
+      errorMessage: "Phone number is required and should be 10 digits only",
+      required: true,
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password",
+      errorMessage:
+        "Password must be 8-15 characters and one special character",
+      required: true,
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      label: "Confirm Password",
+      errorMessage: "Conform password does not match the password",
+      patter: values.password,
+      required: true,
+    },
+  ];
+
+  const handleSubmit = () => {};
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   return (
     <div className="app">
-      <form onSubmit={handleSubmit}>
-        <FormInput name="username" placeholder="Username" />
-        <FormInput name="email" placeholder="Email" />
-        <FormInput name="fullName" placeholder="Full Name" />
-        <FormInput name="phone" placeholder="Phone" />
+      <form className="form" onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
         <button>Submit</button>
       </form>
     </div>
--- react-form/src/FormInput.jsx
+++ react-form/src/FormInput.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormInput = (props) => {
-  const { placeholder, name } = props;
+  const [focus, setFocus] = useState(false);
+  const { onChange, errorMessage, values, label, required, ...extra } = props;
+
+  const handleFocusd = () => {
+    setFocus(true);
+  };
   return (
     <div className="form-input">
-      <label className="form-label">Username</label>
-      <input name={name} placeholder={placeholder} />
+      <label className="form-label">{label}</label>
+      <input
+        {...extra}
+        onChange={onChange}
+        onBlur={handleFocusd}
+        focus={focus.toString()}
+      />
+      <span>{errorMessage}</span>
     </div>
   );
 };
