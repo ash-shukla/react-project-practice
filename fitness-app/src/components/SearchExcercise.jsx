01a4334b6fc0c24dcfa2e25fdfdc06cc0941d2ad
Ashish Shukla <shukla21ashish@gmail.com>
2022-10-20 01:03:55 +0530

    minor chanegs

--- fitness-app/src/components/SearchExcercise.jsx
+++ fitness-app/src/components/SearchExcercise.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { excerciseOptions, fetchData } from "../utils/fetchData";
 
 const SearchExcercise = () => {
-  return <div>SearchExcercise</div>;
+  const [search, setsearch] = useState("");
+  const [excercises, setexcercises] = useState([]);
+  const [bodyParts, setbodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExcerciseList = async () => {
+      const bodyPartData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        excerciseOptions
+      );
+      setbodyParts(["all", ...bodyPartData]);
+    };
+    fetchExcerciseList();
+  }, []);
+
+  const handleSearch = async () => {
+    if (search) {
+      const excerciseData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        excerciseOptions
+      );
+      const searchedExcercises = excerciseData.filter((x) => {
+        x.name.toLowerCase().includes(search) ||
+          x.target.toLowerCase().includes(search) ||
+          x.equipment.toLowerCase().includes(search) ||
+          x.bodyPart.toLowerCase().includes(search);
+      });
+      setsearch("");
+      setexcercises(searchedExcercises);
+      console.log(excerciseData);
+    }
+  };
+
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="80px"
+        textAlign="center"
+      >
+        Awesome Excercises you
+        <br /> should know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{ width: { lg: "1006px", xs: "350px" } }}
+          value={search}
+          onChange={(e) => setsearch(e.target.value.toLowerCase())}
+          placeholder="Search Excercise"
+        />
+        <Button
+          onClick={handleSearch}
+          sx={{ height: "56px", position: "absolute", right: "0" }}
+          color="error"
+          variant="contained"
+        >
+          Search
+        </Button>
+      </Box>
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        {/* <HorizontalScrollBar bodyParts={bodyParts} /> */}
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExcercise;
---
+++ fitness-app/src/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const excerciseOptions = {
+  method: "GET",
+  // url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+  headers: {
+    "X-RapidAPI-Key": "093b074c8fmshc1929c059b50834p1688fajsn7bf39ee12995",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
