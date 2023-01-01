2dc33e5de9c0294bdc73c5aa2af15a7df9516404
Ashish Shukla <shukla21ashish@gmail.com>
2023-01-02 00:51:18 +0530

    initial commit

---
+++ e-commerce/client/src/App.js
@@ -0,0 +1,38 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/home/Home";
+import Product from "./pages/product/Product";
+import Products from "./pages/products/Products";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/products/:id", element: <Products /> },
+      { path: "/product/:id", element: <Product /> },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
