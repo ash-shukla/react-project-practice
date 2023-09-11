71f09ac91c6573d4ee1bb78b034c495486cb2f9d
Ashish Shukla <shukla21ashish@gmail.com>
2023-09-12 01:31:01 +0530

    changes

---
+++ e-commerce-udemy/frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,69 @@
+import { Link, useParams } from "react-router-dom";
+import products from "../products";
+import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
+import Ratings from "../components/Ratings";
+
+const ProductScreen = () => {
+  const { id: productId } = useParams();
+  const product = products?.find((x) => x._id === productId);
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product?.image} alt={product?.image} fluid />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product?.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Ratings
+                value={product?.rating}
+                text={`${product?.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ₹{product?.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product?.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>₹{product?.price}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      {product?.countInStock > 0 ? "In Stock" : "Out of Stocks"}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product?.countInStock === 0}
+                >
+                  {product?.countInStock ? "Add to Cart" : "Out of Stock"}
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
